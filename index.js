ff85f3cdfadc0601e1040d0cd7769f4bbaa019d7
muhammedfawas <58069969+muhammedfawas@users.noreply.github.com>
2019-12-03 21:38:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function add1(){
 var a=document.getElementById("rupee").value;
-document.getElementById("1").value=a*71.61;
-document.getElementById("2").value=a*19.9;
-document.getElementById("3").value=a*48.94;
-document.getElementById("4").value=a*19.68;
-document.getElementById("5").value=a*245.48;
-document.getElementById("6").value=a*52.46;
-document.getElementById("7").value=a*10.17;
+document.getElementById("1").value=a/71.61;
+document.getElementById("2").value=a/19.9;
+document.getElementById("3").value=a/48.94;
+document.getElementById("4").value=a/19.68;
+document.getElementById("5").value=a/245.48;
+document.getElementById("6").value=a/52.46;
+document.getElementById("7").value=a/10.17;
 
 
 }
